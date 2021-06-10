abd83f531b113ee9f2d251fc82fe3c1d267a5d2a
UlaNawrot <ula.nawrot@gmail.com>
2021-06-10 10:38:12 +0200

    adding force:true to click search button

--- cypress/integration/travelApp.spec.js
+++ cypress/integration/travelApp.spec.js
@@ -29,31 +29,31 @@ describe("Booking a hotel in Poland", () => {
     cy.get("#people").select("2");
   });
   //code which doesn't work
-  //   it("should click on SEARCH button",()=>{
-  //     cy.get('.search__btn').click()
-  //   })
+  it("should click on SEARCH button", () => {
+    cy.get(".search__btn").click({ force: true });
+  });
   //Second page
   //   it("should click on BOOK button on second hotel", () => {
   //     cy.get(".hotels > :nth-child(2) > .booking > .btn").click();
   //   });
   //Third page
-//   it("should add taxi to the hotel as an additional option", () => {
-//     cy.get(".options > :nth-child(2)").click;
-//   });
-//   it("should type name and surname", () => {
-//     cy.get("#surname").type("Urszula Nawrot");
-//   });
-//   it("should type email address", () => {
-//     cy.get("#email").type("imie.nazwisko@gmail.com");
-//   });
-//   it("should select bank transfer option", () => {
-//     cy.get("#bank_transfer").click();
-//   });
-//   it("should click on CONFIRM button", () => {
-//     cy.get("#confirm").click();
-//   });
-//   //Page 4
-//   it("should close modal window", () => {
-//     cy.get(".close-modal").click();
-//   });
+  //   it("should add taxi to the hotel as an additional option", () => {
+  //     cy.get(".options > :nth-child(2)").click;
+  //   });
+  //   it("should type name and surname", () => {
+  //     cy.get("#surname").type("Urszula Nawrot");
+  //   });
+  //   it("should type email address", () => {
+  //     cy.get("#email").type("imie.nazwisko@gmail.com");
+  //   });
+  //   it("should select bank transfer option", () => {
+  //     cy.get("#bank_transfer").click();
+  //   });
+  //   it("should click on CONFIRM button", () => {
+  //     cy.get("#confirm").click();
+  //   });
+  //   //Page 4
+  //   it("should close modal window", () => {
+  //     cy.get(".close-modal").click();
+  //   });
 });
